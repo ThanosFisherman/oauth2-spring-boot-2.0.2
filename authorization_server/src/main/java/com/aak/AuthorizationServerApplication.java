ae7372d7ad15fdb9aad8ebb609123c8ced84d851
Thanos Psaridis <psaridis@gmail.com>
2018-09-15 02:02:13 +0300

    minor fixes

--- authorization_server/src/main/java/com/aak/AuthorizationServerApplication.java
+++ authorization_server/src/main/java/com/aak/AuthorizationServerApplication.java
@@ -1,16 +1,10 @@
 package com.aak;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 
 @SpringBootApplication
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
 public class AuthorizationServerApplication {
 
 	public static void main(String[] args) {
